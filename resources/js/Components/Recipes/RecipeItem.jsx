153d2769bd2f59a83132f7eb90a18611a7b29b39
Denice van den Steen <vandensteendenice@gmail.com>
2022-08-07 21:13:49 +0200

    Add styling to recipe list and item

--- resources/js/Components/Recipes/RecipeItem.jsx
+++ resources/js/Components/Recipes/RecipeItem.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
-import { Link, Head } from '@inertiajs/inertia-react';
 import recipeImage from '../../../assets/Recipe_img.png'
+import RecipeList from './RecipeList';
 
 const RecipeItem = ({title, course, duration}) => {
   return (
-    <div className='py-4 px-10'>
-      <div className='flex'>
-        <div className='border'>
-          <div className='flex flex-row py-2 px-2'>
+    <div className='py-4 px-2'>
+      <div className='w-11/12'>
+        <div className='flex flex-row py-2 px-2'>
 
-            <div className='px-4'>
-              <img src={recipeImage} className="w-16 h-12" />
-            </div>
-
-            <div className='flex flex-col'>
-              <div className='px-2 font-bold'>
-                {title}
-              </div>
-            
-              <div className='flex flex-row'>
-                <div className='px-2'>
-                  {course}
-                </div>
-                <div className='px-2'>
-                  {duration}
-                </div>
-              </div>
-            </div>
-
+          <div className='px-4'>
+            <img src={recipeImage} className="w-16 h-12 rounded-sm" />
           </div>
+
+          <div className='flex flex-col'>
+            <div className='px-2 font-bold'>
+              {title}
+            </div>
+          
+            <div className='flex flex-row'>
+              <div className='px-2'>
+                {course}
+              </div>
+              <div className='px-2'>
+                ⏱ {duration} minutes
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
--- resources/js/Components/Recipes/RecipeList.jsx
+++ resources/js/Components/Recipes/RecipeList.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-
 import RecipeItem from './RecipeItem'
 
 const RecipeList = ({recipes}) => {
   return (
-    <div className='py-4 px-10'>
-      <div>
-        {recipes.map((recipe) => {
-          return(
-            <RecipeItem
-              key={recipe.uuid} 
-              title={recipe.title}
-              course="Main"
-              duration={recipe.duration_minutes}
-            />
-          )
-        })}
+    <div className='py-8 px-10'>
+      <div className='w-1/2'>
+        <div className='bg-white rounded-lg drop-shadow-lg'>
+          {recipes.map((recipe, index) => {
+            return(
+              <div className='flex flex-col'>
+                <RecipeItem
+                  key={recipe.uuid} 
+                  title={recipe.title}
+                  course="Main"
+                  duration={recipe.duration_minutes}
+                />
+                <hr className={`${(index + 1 === recipes.length) ? "hidden" : "mx-4"}`} />
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   );
